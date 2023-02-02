d9b35e619789976ba8abe92d6083a9e67334d5b9
Brandon Dalton <dabigin@gmail.com>
2023-02-02 02:15:26 -0600

    finished Section 23 Leson 232 Default Params

--- DefaultParams/app.js
+++ DefaultParams/app.js
@@ -1,3 +1,14 @@
-function rollDie(numSides) {
-  return Math.floor(math.random() * numSides) + 1;
+// function rollDie(numSides) {
+//     if(numSides === undefined){
+//         numSides = 6;
+//     }
+//   return Math.floor(Math.random() * numSides) + 1;
+// }
+
+function rollDie(numSides = 6) {
+  return Math.floor(Math.random() * numSides) + 1;
+}
+
+function greet(person, msg = "Hey there", punc = "!") {
+  console.log(`${msg}, ${person}${punc}`);
 }
